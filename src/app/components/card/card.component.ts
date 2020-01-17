7500d0197024707565edb6da2677fb1dfd711c45
Jc <jcpmandawe@gmail.com>
2020-01-17 11:47:07 +0800

    Add cards mock backend

--- src/app/components/card/card.component.ts
+++ src/app/components/card/card.component.ts
@@ -6,6 +6,23 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./card.component.css']
 })
 export class CardComponent implements OnInit {
+  cards = [
+    {
+      title: "Post 1",
+      description: "Some quick example text to build on the card title and make up the bulk of the card's content.",
+      link: "#",
+    },
+    {
+      title: "Post 2",
+      description: "Some quick example text to build on the card title and make up the bulk of the card's content.",
+      link: "#",
+    },
+    {
+      title: "Post 3",
+      description: "Some quick example text to build on the card title and make up the bulk of the card's content.",
+      link: "#",
+    }
+  ];
 
   constructor() { }
 
