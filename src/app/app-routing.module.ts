86d19261be97def58d87a3cb83338caaf869409f
Jc <jcpmandawe@gmail.com>
2020-01-20 13:57:48 +0800

    Import and add blog route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,12 +3,14 @@ import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
 import { AboutComponent } from './components/about/about.component';
 import { ContactComponent } from './components/contact/contact.component';
+import { BlogComponent } from './components/blog/blog.component';
 
 
 const routes: Routes = [
   { path: "", component: HomeComponent },
   { path: "about", component: AboutComponent },
-  { path: "contact", component: ContactComponent }
+  { path: "blog", component: BlogComponent},
+  { path: "contact", component: ContactComponent },
 ];
 
 @NgModule({
