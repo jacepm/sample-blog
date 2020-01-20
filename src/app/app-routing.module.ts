86499afddb986dd4d6633cb4541f3209709ca391
Jc <jcpmandawe@gmail.com>
2020-01-20 16:54:17 +0800

    Import and add LoginComponent in routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,12 +3,14 @@ import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
 import { BlogComponent } from './components/blog/blog.component';
 import { BlogViewComponent } from './components/blog/blog-view/blog-view.component';
+import { LoginComponent } from './components/login/login.component';
 
 
 const routes: Routes = [
   { path: "", component: HomeComponent },
   { path: "blog", component: BlogComponent},
-  { path: "blog/:id", component: BlogViewComponent}
+  { path: "blog/:id", component: BlogViewComponent},
+  { path: "login", component: LoginComponent}
 ];
 
 @NgModule({
