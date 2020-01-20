d92d62881370460b8eb7d76cc736015bf5bc1b73
Jc <jcpmandawe@gmail.com>
2020-01-20 16:31:16 +0800

    Add blog-view components

---
+++ src/app/components/blog/blog-view/blog-view.component.ts
@@ -0,0 +1,36 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { ApiService } from '../../../services/api.service';
+
+@Component({
+  selector: 'app-blog-view',
+  templateUrl: './blog-view.component.html',
+  styleUrls: ['./blog-view.component.css']
+})
+export class BlogViewComponent implements OnInit {
+  id: any;
+  row: any;
+
+  constructor(
+    private route: ActivatedRoute,
+    private api: ApiService
+  ) { }
+
+  ngOnInit() {
+    this.id = this.route.snapshot.params["id"];
+    this.getBlog();
+  }
+
+  getBlog() {
+    this.api
+      .get("/blog/" + this.id)
+      .then((res: any) => {
+        this.row = res.data;
+        console.log(this.row);
+      })
+      .catch(error => {
+        console.log(error);
+      });
+  }
+
+}
