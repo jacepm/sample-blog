666486e6ca77845b6a43e57190c345c572b57821
Jc <jcpmandawe@gmail.com>
2020-01-20 21:14:53 +0800

    Add createForm, isControlInvalid & login function

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ApiService } from '../../services/api.service';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -6,10 +8,41 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
+  loginForm: FormGroup;
 
-  constructor() { }
+  constructor(
+    private api: ApiService,
+    private fb: FormBuilder
+  ) { }
 
   ngOnInit() {
+    this.createForm();
+  }
+
+  createForm() {
+    this.loginForm = this.fb.group({
+      userName: ["", Validators.required],
+      password: ["", Validators.required]
+    });
+  }
+
+  isControlInvalid(controlName: string): boolean {
+    const control = this.loginForm.controls[controlName];
+    const result = control.invalid && control.touched;
+    return result;
+  }
+
+  login(value: any) {
+    console.log(value);
+    this.api
+      .post("/user/login", value)
+      .then((res: any) => {
+        console.log(res.data);
+        localStorage.setItem("token", res.data.token); 
+      })
+      .catch(error => {
+        console.log(error);
+      });
   }
 
 }
