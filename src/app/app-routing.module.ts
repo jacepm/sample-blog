5fa4893c4748111b42995c77b684cb54230f2da3
Jc <jcpmandawe@gmail.com>
2020-01-20 15:56:47 +0800

    Import and add BlogViewComponent route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,13 +4,15 @@ import { HomeComponent } from './components/home/home.component';
 import { AboutComponent } from './components/about/about.component';
 import { ContactComponent } from './components/contact/contact.component';
 import { BlogComponent } from './components/blog/blog.component';
+import { BlogViewComponent } from './components/blog/blog-view/blog-view.component';
 
 
 const routes: Routes = [
   { path: "", component: HomeComponent },
   { path: "about", component: AboutComponent },
   { path: "blog", component: BlogComponent},
-  { path: "contact", component: ContactComponent },
+  { path: "blog/:id", component: BlogViewComponent},
+  { path: "contact", component: ContactComponent }
 ];
 
 @NgModule({
