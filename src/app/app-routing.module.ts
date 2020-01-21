e16438e22523fec873d7c6f853510c74bd77ae40
Jc <jcpmandawe@gmail.com>
2020-01-21 18:19:45 +0800

    Add AuthGuard in blog routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,13 +4,28 @@ import { HomeComponent } from './components/home/home.component';
 import { BlogComponent } from './components/blog/blog.component';
 import { BlogViewComponent } from './components/blog/blog-view/blog-view.component';
 import { LoginComponent } from './components/login/login.component';
+import { AuthGuard } from './guards/auth.guard';
 
 
 const routes: Routes = [
-  { path: "", component: HomeComponent },
-  { path: "blog", component: BlogComponent},
-  { path: "blog/:id", component: BlogViewComponent},
-  { path: "login", component: LoginComponent}
+  {
+    path: "",
+    component: HomeComponent
+  },
+  {
+    path: "blog",
+    component: BlogComponent,
+    canActivate: [AuthGuard]
+  },
+  {
+    path: "blog/:id",
+    component: BlogViewComponent,
+    canActivate: [AuthGuard]
+  },
+  {
+    path: "login",
+    component: LoginComponent
+  }
 ];
 
 @NgModule({
