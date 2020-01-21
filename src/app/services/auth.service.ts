9354b824dc0d214f77ba15f6b3d12ffe8e1ae67a
Jc <jcpmandawe@gmail.com>
2020-01-21 20:30:18 +0800

    Add logout function

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,14 +1,20 @@
 import { Injectable } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
 
-  constructor() { }
+  constructor(private router: Router) { }
 
   loggedIn() {
     return !!localStorage.getItem("token");
+  }
+
+  logout() {
+    localStorage.removeItem("token");
+    this.router.navigate(["/login"]);
   }
 
   getToken() {
