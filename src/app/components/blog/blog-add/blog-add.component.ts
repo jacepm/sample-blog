5d8dd19bd0ac3663f84b038b19932f36a4295afc
Jc <jcpmandawe@gmail.com>
2020-01-21 21:50:00 +0800

    Add submit function

--- src/app/components/blog/blog-add/blog-add.component.ts
+++ src/app/components/blog/blog-add/blog-add.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder } from '@angular/forms';
+import { ApiService } from 'src/app/services/api.service';
 
 @Component({
   selector: 'app-blog-add',
@@ -6,10 +8,25 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./blog-add.component.css']
 })
 export class BlogAddComponent implements OnInit {
+  blogForm: FormGroup
 
-  constructor() { }
+  constructor(
+    private fb: FormBuilder,
+    private api: ApiService
+  ) { }
 
   ngOnInit() {
+  }
+
+  submit(value: string) {
+    this.api
+      .post("/blog", value)
+      .then((res: any) => {
+        console.log(res.data);
+      })
+      .catch(error => {
+        console.log(error)
+      });
   }
 
 }
