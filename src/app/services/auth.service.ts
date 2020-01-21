88645578ba17ae5b020dee7add786f3083b53bcb
Jc <jcpmandawe@gmail.com>
2020-01-21 20:44:49 +0800

    Fix code

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -14,7 +14,7 @@ export class AuthService {
 
   logout() {
     localStorage.removeItem("token");
-    this.router.navigate(["/login"]);
+    this.router.navigate([`/login`]);
   }
 
   getToken() {
