a8194102cd2621f51982f2f2c61afe07c7e378cd
Jc <jcpmandawe@gmail.com>
2020-01-22 19:50:55 +0800

    Add deleteModal function

--- src/app/components/blog/blog-list/blog-list.component.ts
+++ src/app/components/blog/blog-list/blog-list.component.ts
@@ -1,7 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, TemplateRef } from '@angular/core';
 import { ApiService } from '../../../services/api.service';
 import { Router } from '@angular/router';
 import { AuthService } from '../../../services/auth.service';
+import { DeleteModalComponent } from 'src/app/utilities/delete-modal/delete-modal.component';
+import { BsModalService, BsModalRef } from 'ngx-bootstrap';
 
 @Component({
   selector: 'app-blog-list',
@@ -10,33 +12,35 @@ import { AuthService } from '../../../services/auth.service';
 })
 export class BlogListComponent implements OnInit {
   rows: any;
+  modalRef: BsModalRef;
 
   constructor(
     private api: ApiService,
     public auth: AuthService,
-    private router: Router
-  ) {}
+    private router: Router,
+    private modal: BsModalService
+  ) { }
 
-ngOnInit() {
-  this.getBlogLists();
-}
+  ngOnInit() {
+    this.getBlogLists();
+  }
 
-getBlogLists() {
-  this.api
-    .get("/blog")
-    .then((res: any) => {
-      this.rows = res.data
-        .sort((a, b) =>
-          new Date(a.date) < new Date(b.date) ? 1 : -1
-        )
-        .filter((data: any) =>
-          data.deleted === false
-        );
-      console.log(this.rows);
-    })
-    .catch(error => {
-      console.log(error);
-    });
+  getBlogLists() {
+    this.api
+      .get("/blog")
+      .then((res: any) => {
+        this.rows = res.data
+          .sort((a, b) =>
+            new Date(a.date) < new Date(b.date) ? 1 : -1
+          )
+          .filter((data: any) =>
+            data.deleted === false
+          );
+        console.log(this.rows);
+      })
+      .catch(error => {
+        console.log(error);
+      });
   }
 
   goto(value: string) {
@@ -51,7 +55,18 @@ getBlogLists() {
     this.router.navigate([`/blog/edit/${id}`]);
   }
 
-  delete(id: string) {
+  deleteModal(id: string) {
+    const initialState = {
+      data: "Are you sure you want to delete this blog?",
+      title: "Delete blog"
+    };
+    this.modalRef = this.modal.show(DeleteModalComponent, { initialState });
+    this.modalRef.content.actionBtnName = "Delete";
+    this.modalRef.content.actionBtnColor = "danger";
+    this.modalRef.content.closeBtnName = "Close";
+  }
+
+  /* delete(id: string) {
     const value = { deleted: true }
     this.api
       .patch("/blog/" + id, value)
@@ -62,5 +77,5 @@ getBlogLists() {
       .catch(error => {
         console.log(error);
       });
-  }
+  } */
 }
